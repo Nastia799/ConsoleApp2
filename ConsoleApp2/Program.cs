fd9a65abbcec412cf445ab93586882a1e6fc26f6
Ася <@DESKTOP-EBI5UBR>
2022-03-29 16:55:56 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Address someAddress = new address();
+            some address.  Index = 18766; 16 19 30 someAddress.Country = "Ukraine"; someAddress.city = "Kyiv"; someAddress.Street = "Kolotishkina"; some address.  House = 3; someAddress.Apartment = 66; Console.WriteLine(someAddress.GetAddress()); Console.ReadKey(); i No problems found.  100 % Conclusion Version control system - Git
+        }
+    }
+}
